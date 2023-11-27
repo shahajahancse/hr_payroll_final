a1db6466b500110aaa1694a07da08773785de9fe
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-11-27 16:50:50 +0600

    Some Changes From Apon

--- application/views/setup/line_list.php
+++ application/views/setup/line_list.php
@@ -1,8 +1,7 @@
 <div class="content">
-
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
+<nav class="navbar navbar-inverse bg_none">
+        <div class="container-fluid nav_head">
+            <div class="navbar-header col-md-5" style="padding: 7px;">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
                     aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
@@ -10,22 +9,20 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?php echo base_url('index.php/setup_con/line_add') ?>">Add Line</a>
+                <div>
+                    <a class="btn btn-info" href="<?php echo base_url('index.php/setup_con/line_add') ?>">Add Line</a>
+                            <a class="btn btn-primary" href="<?php echo base_url('index.php/payroll_con') ?>">Home</a>
+                </div>
             </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="<?php echo base_url('index.php/payroll_con') ?>">Home</a></li>
-                </ul>
-                <div class="pull-right">
-                    <form class="navbar-form pull-right" role="search">
-                        <div class="input-group">
-                            <input id="deptSearch" type="text" class="form-control" placeholder="Search">
-                            <div class="input-group-btn">
-                                <button type="submit" class="btn btn-default"><span
-                                        class="glyphicon glyphicon-search"></span></button>
+            <div class="col-md-7">
+                <div id="navbar" class="navbar-collapse collapse">
+                    <div class="">
+                        <form class="navbar-form pull-right" role="search">
+                            <div class="input-group">
+                                <input id="deptSearch" type="text" class="form-control" placeholder="Search">
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
             <!--/.nav-collapse -->
@@ -33,37 +30,10 @@
         <!--/.container-fluid -->
     </nav>
     <div class="row">
-        <div class="col-md-12">
-            <?php
-$success = $this->session->flashdata('success');
-if ($success != "") {
-    ?>
-            <div class="alert alert-success"><?php echo $success; ?></div>
-            <?php
-}
-$failuer = $this->session->flashdata('failuer');
-if ($failuer) {
-    ?>
-            <div class="alert alert-failuer"><?php echo $failuer; ?></div>
-            <?php
-}
-?>
-
-        </div>
+        <?php
+            $failuer = $this->session->flashdata('failure');
+            ?>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-6">
-                <h3 style="margin-top: 0px; margin-bottom: 8px;">Line List</h3>
-            </div>
-            <div class="col-md-6 text-right">
-                <a href="<?=base_url('index.php/setup_con/line_add')?>"  class="btn btn-info"
-                    role="button">Add Line</a>
-            </div>
-        </div>
-    </div>
-
-    <!-- <br> -->
     <div class="row tablebox">
 
         <div class="col-md-12">
@@ -74,6 +44,9 @@ if ($failuer) {
                         <th>SL</th>
                         <th>Line Name English </th>
                         <th>Line Name Bangla </th>
+                        <th>Unit Name </th>
+                        <th>Department Name </th>
+                        <th>Section Name</th>
                         <th width="80">Edit</th>
                         <th>Delete</th>
 
@@ -92,6 +65,9 @@ if ($failuer) {
                         <td><?php echo $key + 1  ?></td>
                         <td><?php echo $pr_lines['line_name_en'] ?></td>
                         <td><?php echo $pr_lines['line_name_bn'] ?></td>
+                        <td><?php echo $pr_lines['unit_name'] ?></td>
+                        <td><?php echo $pr_lines['dept_name'] ?></td>
+                        <td><?php echo $pr_lines['sec_name_en'] ?></td>
                         <td>
                             <a href="<?=base_url('index.php/setup_con/line_edit') . '/' . $pr_lines["id"]?>"
                                  class="btn btn-primary" role="button">Edit</a>
