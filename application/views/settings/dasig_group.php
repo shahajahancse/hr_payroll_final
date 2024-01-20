6e12a746a9a8b8c1965545e004d72b1ed595b78a
Md Shahajahan Ali <msacse1@gmail.com>
2024-01-20 13:49:06 +0600

    msa

--- application/views/settings/dasig_group.php
+++ application/views/settings/dasig_group.php
@@ -78,9 +78,9 @@
                     </select>
                 </div>
             </div>
-        <!-- </div>
-        <div class="row"> -->
-            <div class="col-md-2 pull-right">
+        </div>
+        <div class="row">
+            <div class="col-md-2 pull-right" style="top: -15px !important; right: -60px !important;">
                 <div class="form-group">
                     <label for="acl_name" style="visibility: hidden">.</label>
                     <input type="submit" value="Submit" class="btn btn-success">
