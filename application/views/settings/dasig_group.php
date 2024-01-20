81b12454cdec515eef03778fa8e55d81d209e7d4
Md Shahajahan Ali <msacse1@gmail.com>
2024-01-20 14:33:17 +0600

    msa

--- application/views/settings/dasig_group.php
+++ application/views/settings/dasig_group.php
@@ -118,7 +118,7 @@
                     <td><?= $row->unit_id ?></td>
                     <td>
                         <a class="btn btn-primary"> Edit </a>
-                        <a class="btn btn-info"> Edit </a>
+                        <a class="btn btn-info"> Manage </a>
                     </td>
                 </tr>
             <?php } ?>
