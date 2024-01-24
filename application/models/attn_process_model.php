4e960e099c30fde80658cdeadb9a7c0b566cff7f
Md Shahajahan Ali <msacse1@gmail.com>
2024-01-24 11:25:23 +0600

    msa

--- application/models/attn_process_model.php
+++ application/models/attn_process_model.php
@@ -206,7 +206,7 @@ class Attn_process_model extends CI_Model{
 
 							// OT Calculation
 							$ot_hour = floor($minute / 60);
-							dd($ot_hour .' = '. $minute .' = '. $ot_last_hour);
+							dd($minute % 60);
 							if ($minute % 60 >= $ot_last_hour) {
 								$ot_hour = $ot_hour + 1;
 							}
