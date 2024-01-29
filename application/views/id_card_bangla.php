b2cff51b19efa4bb7ca9e5d9d3e68ef980b4c811
Mohammad Shakil Hossain <shakil.mysoftheaven@gmail.com>
2024-01-29 12:51:55 +0600

    changed

--- application/views/id_card_bangla.php
+++ application/views/id_card_bangla.php
@@ -19,8 +19,8 @@ body {
 
 .box {
   width: 440px;
-  height: 250px;
-  margin: 20px;
+  height: 265px;
+  margin: 12px;
   float: left;
   background-color: #ffffff;
   border: 1px solid #000000;
@@ -40,7 +40,7 @@ body {
 p {
   margin: 0;
   padding: 0;
-  font-size: 18px;
+  font-size: 20.6px;
 }
 
 .box-top {
@@ -53,7 +53,7 @@ p {
   top: 38px;
   right: 10px;
   width: 106px;
-  height: 116px;
+  height: 119px;
 
 }
 
@@ -71,7 +71,7 @@ p {
 </head>
 
 
-<body>
+<body style="line-height:1.4 !important">
   <!-- < ?php dd($values)?> -->
   <div class="container">
     <?php foreach($values as $value ){?>
@@ -88,14 +88,14 @@ p {
       <h4 class="col-md-12 text-center" style="margin-top:-18px"><b>cwiPq cÎ</b></h4>
         <div class="ml-2">
           <img src="<?php echo base_url('/uploads'.'/photo/'.$value->img_source)?>" alt="" class="box-img" style="border:1px solid black">
-          <p class="box-top ">AvBwW KvW© bst <span style="font-size:16px"> <b><?php echo $value->emp_id?></b></span></p>
-          <p class="box-top"> Bmyy¨i ZvwiL t <span style="font-size:16px"> <b><?php echo date('d-m-Y',strtotime($value->emp_join_date))?> Bs</b></span></p>
+          <p class="box-top ">AvBwW KvW© bst <span style="font-size:19px"> <b><?php echo $value->emp_id?></b></span></p>
+          <p class="box-top"> Bmyy¨i ZvwiL t <span style="font-size:19px"> <b><?php echo date('d-m-Y',strtotime($value->emp_join_date))?> Bs</b></span></p>
           <p class="box-top">bvg t <span style="font-size:12px"><b><?php echo $value->name_bn?></b></span></p>
           <p class="box-top">c`ex t <span style="font-size:12px"><b><?php echo $value->desig_bangla?></b></span></p>
           <p class="box-top"> wefvM/kvLvt <span style="font-size:12px"><b><?php echo $value->sec_name_bn?></b></span></p>
           <p class="box-top"> <span>Kv‡Ri aibt <span style="font-size:12px"><b><?php echo $value->emp_cat_id ==1 ? "স্থায়ী" : "SS"?></b></span></span></p>
           <p class="box-top">jvBbt <span style="font-size:12px"><b><?php echo $value->line_name_bn?></b></span></p>
-          <p class="box-top">‡hvM`v‡bi ZvwiLt <b style="font-size:16px"><?php echo date('d-m-Y',strtotime($value->emp_join_date))?> Bs</b></p>
+          <p class="box-top">‡hvM`v‡bi ZvwiLt <b style="font-size:19px"><?php echo date('d-m-Y',strtotime($value->emp_join_date))?> Bs</b></p>
           <img src="<?php echo base_url('/images/'.$image->company_signature)?>" style="width: 18%;position: absolute;margin-top: -18px;right: 20px;">
           <div class="d-flex justify-content-between" style="margin-top: 15px;">
             <p class="box-top mt-2">MÖnbKvixi ¯^vÿi</p>
@@ -109,12 +109,12 @@ p {
       <p class="box-top text-center">‡gqv`t PvKzwi _vKvKvjxb ch©šÍ|</p>
       <p class="box-top text-center">cÖwZôv‡bi wVKvbvt 799, (cyivZb cøU bs- 1010/1011), AvgevM,‡gŠRv evwNqv, ‡Kvbvevox, MvRxcyi -1700|</p>
       <p class="box-top text-center">‡dvb bst 09611677670, 01749087002</p>
-      <p class="box-top text-center">D³ cwiPq cÎ nvivBqv †M‡j ZvrÿwbK e¨e¯’vcbv KZ…©cÿ‡K RvbvB‡Z</p> <p class="box-top text-center">n‡e|</p>
+      <p class="box-top text-center">D³ cwiPq cÎ nvivBqv †M‡j ZvrÿwbK e¨e¯’vcbv KZ…©cÿ‡K RvbvB‡Z n‡e|</p>
       <p class="box-top text-center">i‡³i MÖæct <spans style="font-family: Arial, Helvetica, sans-serif; font-size:14px"> <b><?php echo $value->blood_name?></b></spans></p>
       <p class="box-top text-center">¯’vqx wVKvbv :  MÖvg : <span style="font-size:12px;font-weight:bold"><?php echo $value->per_village_bn?></span>,WvKNi : <span style="font-size:12px;font-weight:bold"><?php echo $value->post_name_bn?></span></p>
       <p class="box-top text-center"> _vbv :<span style="font-size:12px;font-weight:bold"><?php echo $value->upa_name_bn?></span>, ‡Rjv :<span style="font-size:12px;font-weight:bold"> <?php echo $value->dis_name_bn?></span></p>
-      <p class="box-top text-center">Riyix ‡hvMv‡hv‡Mi ‡dvb bst <span style="font-size:16px"><b><?php echo $value->bank_bkash_no?></b></span></p>
-      <p class="box-top text-center"> <?php echo $value->nid_dob_check == 1 ? 'RvZxq cwiPq cÎ bst' : 'Rb¥wbeÜb bst'?> <span style="font-size:16px"> <b><?php echo $value->nid_dob_id?></b></span></p>
+      <p class="box-top text-center">Riyix ‡hvMv‡hv‡Mi ‡dvb bst <span style="font-size:19px"><b><?php echo $value->bank_bkash_no?></b></span></p>
+      <p class="box-top text-center"> <?php echo $value->nid_dob_check == 1 ? 'RvZxq cwiPq cÎ bst' : 'Rb¥wbeÜb bst'?> <span style="font-size:19px"> <b><?php echo $value->nid_dob_id?></b></span></p>
     </div>
     <?php }?>
   </div>
