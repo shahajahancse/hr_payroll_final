db0f601e742cfd332e6c520a3bd75a8d66488f7d
Mohammad Shakil Hossain <shakil.mysoftheaven@gmail.com>
2024-03-27 14:57:20 +0600

    changed

--- application/views/attn_report/daily_costing_summary.php
+++ application/views/attn_report/daily_costing_summary.php
@@ -63,7 +63,7 @@ $num_of_days 	= date("t",strtotime($grid_date));
         <td style="padding:5px;text-align:center"><?php echo round($item->present_gross_salary / $num_of_days); @$perday_salary += round($item->present_gross_salary / $num_of_days)?></td>
         <td style="padding:5px;text-align:center"><?php echo $item->ot;@$total_ot +=$item->ot ?></td>
         <td style="padding:5px;text-align:center"><?php echo $item->eot; @$total_eot +=$item->eot?></td>
-        <td style="padding:5px;text-align:center"><?php echo round(($item->present_gross_salary/104)*$item->ot/$item->present_emp,2); @$total_ot_amount +=round($item->present_gross_salary/104,2)*$item->ot?></td>
+        <td style="padding:5px;text-align:center"><?php echo (($item->present_emp != 0) ? round(($item->present_gross_salary/104)*$item->ot/$item->present_emp,2) : 0); @$total_ot_amount += (($item->present_emp != 0) ? round($item->present_gross_salary/104,2)*$item->ot : 0)?></td>
         <td style="padding:5px;text-align:center"><?php echo round(($item->present_gross_salary/104)*$item->eot/$item->present_emp,2); @$total_eot_amount +=round($item->present_gross_salary/104,2)*$item->eot?></td>
         <td style="padding:5px;text-align:center"><?php echo $item->night_allowence; @$total_night_amount += $item->night_allowence?></td>
         <td style="padding:5px;text-align:center"><?php echo $item->holiday_allowence; @$total_holiday_amount += $item->holiday_allowence?></td>
