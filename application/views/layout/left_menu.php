529ca47fd2018cdb945be937cc519cb8cbe8f098
Md Shahajahan Ali <msacse1@gmail.com>
2024-04-18 17:22:45 +0600

    Some Modified

--- application/views/layout/left_menu.php
+++ application/views/layout/left_menu.php
@@ -127,19 +127,7 @@
           <li class="start <?= activate_method('user_acl_pr') ?>"> <a href="<?=base_url('setting_con/user_acl_pr')?>" class="anchor_cls" id="acl">User Access Payroll</a> </li>
           <li class="start <?= activate_method('dasig_group') ?>"> <a href="<?=base_url('setting_con/dasig_group')?>" class="anchor_cls" id="acl">Group Dasignation</a> </li>
           <li class="start <?= activate_method('hide_designation_employee') ?>"> <a href="<?=base_url('setting_con/hide_designation_employee')?>" class="anchor_cls" id="acl">Hide Designation</a> </li>
-        </ul>
-      </li>
-      <?php } ?>
-
-      <!-- ACL -->
-      <?php if(in_array(17,$acl)) { ?>
-      <li class="start <?= activate_class('acl_con') ?>"> <a href="javascript:;"> <i class="fa fa-cog"></i>
-        <span class="title">Maintenance </span> <span class="selected"></span> <span class="arrow <?= arrow_open('acl_con') ?>"></span> </a>
-        <ul class="sub-menu ">
-
-          <li class="start <?= activate_method('acl') ?>"> <a href="<?=base_url('acl_con/acl')?>" class="anchor_cls" id="acl">User ACL</a> </li>
-          <li class="start <?= activate_method('user_mode') ?>"> <a href="<?=base_url('acl_con/user_mode')?>" class="anchor_cls" id="acl">User Mode</a> </li>
-
+          <li class="start <?= activate_method('acl') ?>"> <a href="<?=base_url('setting_con/acl')?>" class="anchor_cls" id="acl">User ACL</a> </li>
         </ul>
       </li>
       <?php } ?>
