4d9a9785bedb2a6ab94636c914b6ccbaad2213c0
Md Shahajahan Ali <msacse1@gmail.com>
2024-04-23 17:51:08 +0600

    Some Modified

--- application/controllers/Autocomplete.php
+++ application/controllers/Autocomplete.php
@@ -4,19 +4,21 @@ class Autocomplete extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 
-		/* Standard Libraries */
-		$this->data['user_data'] = $this->session->userdata('data');
-		// $this->load->library('grocery_CRUD');
-		// $this->load->model('acl_model');
-		// $this->load->model('common_model');
+		if($this->session->userdata('logged_in')==FALSE)
+		{
+			redirect("authentication");
+		}
 
-        if ($this->session->userdata('logged_in') == false) {
-            redirect("authentication");
-        }
-        $this->data['user_data'] = $this->session->userdata('data');
+		$this->data['user_data'] = $this->session->userdata('data');
+		if (!check_acl_list($this->data['user_data']->id,1)) {
+			echo "<SCRIPT LANGUAGE=\"JavaScript\">alert('Sorry! Acess Deny');</SCRIPT>";
+			redirect("payroll_con");
+			exit;
+		}
 	}
     public function employee_id(){
-        $id=$this->input->post('id');
+        dd($this->data['user_data']);
+        $id = $this->input->post('id');
         $this->db->select('emp_id');
         $this->db->like('emp_id', $id);
         $this->db->limit(70);
@@ -82,6 +84,6 @@ class Autocomplete extends CI_Controller {
 
 
 
-	
+
 }
 
