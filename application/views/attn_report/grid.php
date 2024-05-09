6223f53642658eb576c0dfafc49c58fb706eb896
Md Shahajahan Ali <msacse1@gmail.com>
2024-05-09 14:51:31 +0600

    Some Modified

--- application/views/attn_report/grid.php
+++ application/views/attn_report/grid.php
@@ -200,6 +200,10 @@
 							<?php if(in_array(76,$acl)) { ?>
 							<button class="btn input-sm sbtn" onclick="daily_logout_report()">Daily Logout Report</button>
 							<?php } ?>
+							<?php if(in_array(105,$acl)) { ?>
+							<button class="btn input-sm sbtn" onclick="ot_acknowledgement_sheet(1)">OT Acknowledgement sheet</button>
+							<button class="btn input-sm sbtn" onclick="ot_acknowledgement_sheet(2)">OT Acknowledgement Female</button>
+							<?php } ?>
 						</div>
 						<!-- Daily Reports end -->
 
@@ -262,10 +266,6 @@
 							<?php } ?>
 							<?php if(in_array(104,$acl)) { ?>
 							<button class="btn input-sm sbtn" onclick="last_increment_promotion(2)">Last Promotion Check</button>
-							<?php } ?>
-							<?php if(in_array(105,$acl)) { ?>
-							<button class="btn input-sm sbtn" onclick="ot_acknowledgement_sheet(1)">OT Acknowledgement sheet</button>
-							<button class="btn input-sm sbtn" onclick="ot_acknowledgement_sheet(2)">OT Acknowledgement Female</button>
 							<?php } ?>
 						</div>
 						<!-- Continuous Reports end -->
